c4668dfefbe4a36b66fff84fc933910c9c1bba6d
ryh00 <ryho4913@gmail.com>
2019-09-20 16:09:41 -0600

    Yes

--- lab3_script.sh
+++ lab3_script.sh
@@ -12,10 +12,10 @@ read regex
 #Number 2
 grep $regex $filename
 #Number 3
-egrep -c "~[0-9]{3}-[0-9]{3}-[0-9]{4}~" $filename
+egrep -c "^[0-9]{3}-[0-9]{3}-[0-9]{4}$" $filename
 #Number 4.1
 grep -c ".*@*\.com" $filename
 #Number 4.2
-grep -o "303-[0-9]{3}-[0-9]{4}" $filename
+grep -o "^303-[0-9]{3}-[0-9]{4}$" $filename
 #Number 4.3
-grep @geocities.com $filename > email_results.txt
+grep ".+@geocities.com$" $filename > email_results.txt
