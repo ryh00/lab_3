3fefc0572238d35505ea4f1b933dd7bc6a661418
ryh00 <ryho4913@gmail.com>
2019-09-20 16:05:10 -0600

    Yes

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,8 +4,18 @@
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem
+#Number 1
 echo "Enter a filename"
 read filename
 echo "Enter a regular expression"
 read regex
-grep $regex $filename >> email_results.txt
+#Number 2
+grep $regex $filename
+#Number 3
+egrep -c "~[0-9]{3}-[0-9]{3}-[0-9]{4}~" $filename
+#Number 4.1
+grep -c ".*@*\.com" $filename
+#Number 4.2
+grep -o "303-[0-9]{3}-[0-9]{4}" $filename
+#Number 4.3
+grep @geocities.com $filename > email_results.txt
